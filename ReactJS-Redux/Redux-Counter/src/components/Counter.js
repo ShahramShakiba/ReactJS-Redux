612cb57f2b2af2e695c787f969d2e3d43902f541
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-03-27 12:57:12 +0330

    Dispatching Actions From Inside Components via useDispatch

--- ReactJS-Redux/Redux-Counter/src/components/Counter.js
+++ ReactJS-Redux/Redux-Counter/src/components/Counter.js
@@ -1,8 +1,18 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import classes from './Counter.module.css';
 
 export default function Counter() {
+  // Get access to data that managed in Redux store
   const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  const handleIncrement = () => {
+    dispatch({ type: 'INCREMENT' });
+  };
+  const handleDecrement = () => {
+    dispatch({ type: 'DECREMENT' });
+  };
+
   const toggleCounterHandler = () => {};
 
   return (
@@ -11,7 +21,16 @@ export default function Counter() {
 
       <div className={classes.value}> {counter} </div>
 
+      <div>
+        <button onClick={handleDecrement}> Decrement ⇣ </button>
+        <button onClick={handleIncrement}> ⇡ Increment </button>
+      </div>
+
       <button onClick={toggleCounterHandler}> Toggle Counter </button>
     </main>
   );
 }
+
+/* useSelector
+- React Redux will create a subscription to the Redux store's state for this component, and get the value of `counter` key 
+*/
